c32426151ecad26311c0980c3119ea41162ad820
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2022-05-25 10:12:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
+function TodoApp() {
+  const firstRender = useRef(true);
+
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    if (inputValue.trim() === "") return;
+
+    setTodos([
+      ...todos,
+      {
+        text: inputValue,
+        id: uuidv4(),
+      },
+    ]);
+
+    setInputValue("");
+  };
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  useEffect(() => {
+    if (firstRender.current) {
+      console.log("true");
+      firstRender.current = false;
+    } else {
+      localStorage.setItem("Todo", JSON.stringify([...todos]));
+      console.log("This is not the first page load");
+    }
+  }, [todos]);
+
+  useEffect(() => {
+    if (localStorage.getItem("Todo") !== null) {
+      const newTodos = localStorage.getItem("Todo");
+      setTodos(JSON.parse([...todos, newTodos]));
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>ToDo Example</h1>
+      <form onSubmit={addTodo}>
+        <input
+          autoFocus
+          type="text"
+          placeholder="Add a ToDo"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button>Add ToDo</button>
+      </form>
+      {todos.map((todo) => (
+        <div key={todo.id} className="todo">
+          <p>{todo.text}</p>
+          <button onClick={() => removeTodo(todo.id)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default TodoApp;
